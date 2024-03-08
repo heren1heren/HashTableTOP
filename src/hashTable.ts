2a55e3b0ec41ab6d82d5e200af10bf91034fdea8
heren <heren1heren@gmail.com>
2024-03-08 13:09:09 -0700

    start hashMap project

---
+++ src/hashTable.ts
@@ -0,0 +1,6 @@
+// always using the below restriction as a requirement:
+/**
+ * if (index < 0 || index >= buckets.length) {
+  throw new Error("Trying to access index out of bound");
+}
+ */
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
 // import './style.css';
 import './style.scss';
+// always using the below restriction as a requirement:
+/**
+ * if (index < 0 || index >= buckets.length) {
+  throw new Error("Trying to access index out of bound");
+}
+ */
+
+class HashMap {
+  buckets: Array;
+  constructor() {
+    this.buckets = [];
+  }
+
+  hash(key: any) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+}
+
+let hashMap = new HashMap();
+console.log(hashMap);
